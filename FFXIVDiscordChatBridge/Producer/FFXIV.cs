715959573397dc9b7d0c43daada4a698daf40b3c
Vincent Mahnke <vincent@mahn.ke>
2023-07-22 03:24:55 +0200

    feat: Handle emoji, mentions, attachments and stickers chore: Better handle special characters

--- FFXIVDiscordChatBridge/Producer/FFXIV.cs
+++ FFXIVDiscordChatBridge/Producer/FFXIV.cs
@@ -1,12 +1,15 @@
 ﻿using System.Diagnostics;
 using System.Runtime.InteropServices;
 using Microsoft.Extensions.Logging;
+using WindowsInput;
+using WindowsInput.Native;
 
 namespace FFXIVDiscordChatBridge.Producer;
 
 public class FFXIV : IFFXIVProducer
 {
     private readonly ILogger<FFXIV> _logger;
+    private readonly InputSimulator _inputSimulator = new();
 
     [DllImport ("User32.dll")]
     private static extern int SetForegroundWindow(IntPtr point);
@@ -28,13 +31,17 @@ public class FFXIV : IFFXIVProducer
         }
     }
         
-    public async Task Send(string message)
+    public Task Send(string message)
     {
         _logger.LogInformation("Sending message to FFXIV: {Message}", message);
-        SendKeys.SendWait("~");
-        await Task.Delay(200);
-        SendKeys.SendWait(message);
-        await Task.Delay(500);
-        SendKeys.SendWait("~");
+        _inputSimulator.Keyboard.KeyPress(VirtualKeyCode.RETURN);
+        _inputSimulator.Keyboard.Sleep(300);
+        _inputSimulator.Keyboard.TextEntry(message + "   "); // add a few spaces to prevent the message from being cut off at low frame rates
+        _inputSimulator.Keyboard.Sleep(600);
+        _inputSimulator.Keyboard.KeyPress(VirtualKeyCode.RETURN);
+        _inputSimulator.Keyboard.KeyPress(VirtualKeyCode.ESCAPE);
+        _inputSimulator.Keyboard.KeyPress(VirtualKeyCode.ESCAPE);
+        _inputSimulator.Keyboard.Sleep(300);
+        return Task.CompletedTask;
     }
 }
