79366a66572b4c8f739c7f6b16bea2f0ed376b26
Vincent Mahnke <vincent@mahn.ke>
2023-07-23 23:15:50 +0200

    feat: Translate global Discord emoji into text

---
+++ FFXIVHelpers/DiscordEmojiConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using Discord;
+
+namespace FFXIVHelpers;
+
+public class DiscordEmojiConverter
+{
+    private readonly Dictionary<string, string> _unicodeAndNames = new();
+
+    public DiscordEmojiConverter()
+    {
+        var emojiNamesAndUnicode = typeof(Emoji).GetProperty("NamesAndUnicodes", BindingFlags.NonPublic | BindingFlags.Static)?.GetValue(null) as Dictionary<string, string>;
+        foreach (var (key, value) in emojiNamesAndUnicode ?? Enumerable.Empty<KeyValuePair<string, string>>())
+        {
+            _unicodeAndNames.TryAdd(value, key);
+        }
+    }
+    public string ReplaceEmoji(string input)
+    {
+        var foundCodes = _unicodeAndNames?.Keys.Where(input.Contains).ToList();
+        return (foundCodes ?? Enumerable.Empty<string>()).Aggregate(input, (current, unicode) => current.Replace(unicode, $"{_unicodeAndNames![unicode]}"));
+    }
+}
