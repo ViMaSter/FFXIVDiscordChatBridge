470637e6e7572da0170f8bfba9a78ed9575579c3
Vincent <vincent@mahn.ke>
2023-07-29 20:28:17 +0200

    chore: Added helper console app to convert log-format binary into raw binary files

---
+++ BinaryFromLogGenerator/Program.cs
@@ -0,0 +1,34 @@
+﻿// cocona lite app
+// take these parameters:
+// --inputFile="C:/Users/.../log.txt"
+// --outputFile="C:/Users/.../test.bin"
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using Cocona;
+
+CoconaLiteApp.Run((string inputFile, string outputFile) =>
+{
+ var line = File.ReadAllLines(inputFile).First();
+ var bytes = Snasen.CastFromString(line);
+ 
+ File.WriteAllBytes(outputFile, bytes.ToArray());
+});
+
+static class Snasen
+{
+ // 12-34-56-78-90-AB-CD-EF to byte array
+ public static byte[] CastFromString(string input)
+ { 
+  var split = input.Split('-');
+  var output = new byte[split.Length];
+  for (var i = 0; i < split.Length; i++)
+  {
+   output[i] = byte.Parse(split[i], System.Globalization.NumberStyles.HexNumber);
+  }
+  return output;
+ }   
+}
