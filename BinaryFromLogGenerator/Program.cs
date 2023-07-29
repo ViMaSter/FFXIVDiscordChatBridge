7fb00edfdbdb20e81660f2934c5943606d250802
Vincent <vincent@mahn.ke>
2023-07-29 20:33:42 +0200

    refactor(BinaryFromLogGenerator): Reduces complexity

--- BinaryFromLogGenerator/Program.cs
+++ BinaryFromLogGenerator/Program.cs
@@ -1,34 +1,22 @@
-﻿// cocona lite app
-// take these parameters:
-// --inputFile="C:/Users/.../log.txt"
-// --outputFile="C:/Users/.../test.bin"
-
-using System;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Collections.Generic;
-using Cocona;
-
-CoconaLiteApp.Run((string inputFile, string outputFile) =>
+﻿// takes absolute path to input file and output file
+// input file needs one line formatted of bytes
+// example: 12-34-56-78-90-AB-CD-EF
+Cocona.CoconaLiteApp.Run((string inputFile, string outputFile) =>
 {
- var line = File.ReadAllLines(inputFile).First();
- var bytes = Snasen.CastFromString(line);
- 
- File.WriteAllBytes(outputFile, bytes.ToArray());
-});
-
-static class Snasen
-{
- // 12-34-56-78-90-AB-CD-EF to byte array
- public static byte[] CastFromString(string input)
- { 
+ IEnumerable<byte> CastFromString(string input)
+ {
   var split = input.Split('-');
   var output = new byte[split.Length];
   for (var i = 0; i < split.Length; i++)
   {
    output[i] = byte.Parse(split[i], System.Globalization.NumberStyles.HexNumber);
   }
+
   return output;
- }   
-}
+ }
+
+ var inputLine = File.ReadAllLines(inputFile).First();
+ var bytes = CastFromString(inputLine);
+ 
+ File.WriteAllBytes(outputFile, bytes.ToArray());
+});
